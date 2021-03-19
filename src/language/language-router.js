6567a0d58d9b2f6613732f411d5368317502b2c0
aniledev <elinamcgill@gmail.com>
2021-03-19 02:41:59 -0400

    CHORE: add comment code explaining the language router for the endpoints that are required

--- src/language/language-router.js
+++ src/language/language-router.js
@@ -1,58 +1,61 @@
-const express = require('express')
-const LanguageService = require('./language-service')
-const { requireAuth } = require('../middleware/jwt-auth')
+const express = require("express");
+const LanguageService = require("./language-service");
+const { requireAuth } = require("../middleware/jwt-auth");
 
-const languageRouter = express.Router()
+const languageRouter = express.Router();
 
+// middleware for the languageRouter
 languageRouter
-  .use(requireAuth)
+  .use(requireAuth) // jwt middleware authorization
   .use(async (req, res, next) => {
     try {
+      // gets the specific language for a user in the database using the LanguageService
       const language = await LanguageService.getUsersLanguage(
-        req.app.get('db'),
-        req.user.id,
-      )
+        req.app.get("db"),
+        req.user.id
+      );
 
+      //error handling if there is no language in the database for that user
       if (!language)
         return res.status(404).json({
           error: `You don't have any languages`,
-        })
+        });
 
-      req.language = language
-      next()
+      // the set the req body.language property to a value of the language retrieved from the server
+      req.language = language;
+      next();
     } catch (error) {
-      next(error)
+      next(error);
     }
-  })
+  });
 
-languageRouter
-  .get('/', async (req, res, next) => {
-    try {
-      const words = await LanguageService.getLanguageWords(
-        req.app.get('db'),
-        req.language.id,
-      )
+languageRouter.get("/", async (req, res, next) => {
+  try {
+    // use the LanguageService to get the list of words in the database for that language in the database
+    const words = await LanguageService.getLanguageWords(
+      req.app.get("db"),
+      req.language.id
+    );
 
-      res.json({
-        language: req.language,
-        words,
-      })
-      next()
-    } catch (error) {
-      next(error)
-    }
-  })
+    // returns  a json response oject with the language as a property and the words as anotehr property
+    res.json({
+      language: req.language,
+      words,
+    });
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
-languageRouter
-  .get('/head', async (req, res, next) => {
-    // implement me
-    res.send('implement me!')
-  })
+languageRouter.get("/head", async (req, res, next) => {
+  // implement me
+  res.send("implement me!");
+});
 
-languageRouter
-  .post('/guess', async (req, res, next) => {
-    // implement me
-    res.send('implement me!')
-  })
+languageRouter.post("/guess", async (req, res, next) => {
+  // implement me
+  res.send("implement me!");
+});
 
-module.exports = languageRouter
+module.exports = languageRouter;
